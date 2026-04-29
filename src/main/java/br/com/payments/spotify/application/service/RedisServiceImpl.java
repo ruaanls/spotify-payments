418335f0f590722343af45eeafdf8cde56056b79
ruaanls <ruanlima257@hotmail.com>
2026-04-28 21:45:56 -0300

    Feat: Adicionando variáveis de ambiente pelo keyvault da aws

--- src/main/java/br/com/payments/spotify/application/service/RedisServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package br.com.payments.spotify.application.service;
-
-public interface RedisServiceImpl
-{
-    String getTokenRedis( String id, String type);
-    void saveTokenRedis(String id,String value, String type);
-}
